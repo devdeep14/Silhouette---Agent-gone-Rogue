ce5b12acb575d5b9006bb450e1f7b5b0396da450
devdeep14 <102231701+devdeep14@users.noreply.github.com>
2022-11-20 17:39:43 +0530

    update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class PlayerMovement : MonoBehaviour
     public float jumpPower;
     public GameObject graphics;
     private Animator anim;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -64,5 +67,14 @@ public class PlayerMovement : MonoBehaviour
             isGrounded = true;
             anim.SetBool("isJumping", false);
         }
+        if (collision.gameObject.tag == "end")    //restart code
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
     }
+
+
+    
+
+   
 }
